276d6c12aa0fa3d051a0f5dd03f57dc50aaf2f9d
YashThange1203 <122517657+YashThange1203@users.noreply.github.com>
2025-01-23 02:04:50 +0530

    Partners Logos

--- team58/src/components/Partners.jsx
+++ team58/src/components/Partners.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Partners.css"; // Importing the CSS file for styling
-import NGO1 from "../images/NGO1.png"; // Importing the image file for NGO1
+import NGO1 from "../images/NGO1.png";
+import Resto1 from "../images/resto1.png";
+import NGO2 from "../images/NGO2.png";
+import NGO3 from "../images/NGO3.png";
+import NGO4 from "../images/NGO4.png";
+import NGO5 from "../images/NGO5.png";
+import Resto2 from "../images/resto2.png";
+// Importing the image file for NGO1
 
 const Partners = () => {
   const organizations = [
@@ -8,7 +15,68 @@ const Partners = () => {
       name: "Helping Hands NGO",
       type: "NGO",
       address: "45 Bandra West, Mumbai",
-      image: NGO1, 
+      
+      image: NGO1,
+    },
+    {
+      name: "Happy Meals Restaurant",
+      type: "RESTAURANT",
+      address: "12 Andheri East, Mumbai",
+      donations: 220,
+      image: Resto1,
+    },
+    {
+      name: "Feed the Hungry",
+      type: "NGO",
+      address: "23 Malad West, Mumbai",
+      
+      image: NGO2,
+    },
+    {
+      name: "Sharing Smiles",
+      type: "NGO",
+      address: "34 Borivali East, Mumbai",
+      
+      image: NGO3,
+    },
+    {
+      name: "Feeding India",
+      type: "NGO",
+      address: "56 Kandivali West, Mumbai",
+      
+      image: NGO4,
+    },
+    {
+      name: "Food for Life",
+      type: "NGO",
+      address: "67 Goregaon East, Mumbai",
+      
+      image: NGO5,
+    },
+    {
+      name: "Hunger Bros",
+      type: "RESTAURANT",
+      address: "78 Jogeshwari West, Mumbai",
+      donations:190,
+      image: Resto2,
+    },
+    {
+      name: "Food for Soul",
+      type: "NGO",
+      address: "89 Santacruz East, Mumbai",
+      image: NGO1,
+    },
+    {
+      name: "Feeding the Future",
+      type: "NGO",
+      address: "90 Vile Parle West, Mumbai",
+      image: NGO1,
+    },
+    {
+      name: "Feeding the Needy",
+      type: "NGO",
+      address: "23 Malad West, Mumbai",
+      image: NGO1,
     },
   ];
 
