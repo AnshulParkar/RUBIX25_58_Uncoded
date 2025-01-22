c367db8ecff3aecafdd49e01209f32cd5b2db882
AnshulParkar <121174809+AnshulParkar@users.noreply.github.com>
2025-01-22 14:53:26 +0530

    Update header and hero section styles, replace favicon, and modify navigation links

--- team58/src/components/Header.jsx
+++ team58/src/components/Header.jsx
@@ -8,12 +8,13 @@ function Header() {
       <nav className="nav">
         <ul>
           <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#menu">Menu</a></li>
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#menu">Partners</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
-      <button className="book-button">Donate food</button>
+      {/* <button className="book-button">Donate food</button> */}
+      <button className="login-button">login/signup</button>
     </header>
   );
 }
